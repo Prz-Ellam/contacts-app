6f311d04e83fc3e8dc0433aabec22904f1f24f7f
Prz-Ellam <PerezAlex088@outlook.com>
2023-11-12 22:59:00 -0600

    Initial commit

---
+++ app/src/index.php
@@ -0,0 +1,21 @@
+<?php
+
+session_start();
+
+if (isset($_SESSION["user"])) {
+  header("Location: home.php");
+  return;
+}
+
+?>
+
+<?php require "partials/header.php" ?>
+
+<div class="welcome d-flex align-items-center justify-content-center">
+  <div class="text-center">
+    <h1>Store Your Contacts Now</h1>
+    <a class="btn btn-lg btn-dark" href="register.php">Get Started</a>
+  </div>
+</div>
+
+<?php require "partials/footer.php" ?>
